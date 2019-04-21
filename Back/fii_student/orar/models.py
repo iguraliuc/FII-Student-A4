8fd4f2980b6fc2162bf9b27f3678ee10c87ecb93
cristi <crissduna@gmail.com>
2019-04-21 18:28:16 +0300

    disponibilitate sali alte imbunatatiri

--- Back/fii_student/orar/models.py
+++ Back/fii_student/orar/models.py
@@ -2,43 +2,20 @@ from django.db import models
 
 # Create your models here.
 
-class Sala(models.Model):
-    nume_sala = models.CharField(max_length=15)
-    etaj = models.IntegerField()
-    echipari = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.nume_sala
-
-
-class Materie(models.Model):
-    titlu_curs = models.CharField(max_length=51)
-    credite = models.IntegerField()
-    an = models.IntegerField()
-    semestru = models.IntegerField()
-
-    def __str__(self):
-        return self.titlu_curs
-
-class Profesor(models.Model):
-    nume = models.CharField(max_length=31)
-    prenume = models.CharField(max_length=51)
-    materii = models.ManyToManyField(Materie)
-    grad_didactic = models.CharField(max_length=31)
-    cabinet = models.ForeignKey(Sala, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.nume + self.prenume
 
 class Rand(models.Model):
-    curs = models.ForeignKey(Materie,default=0, on_delete=models.CASCADE, related_query_name='cursul')
+    curs = models.CharField(max_length=100)
     zi = models.CharField(max_length=15)
     ora_inceput = models.TimeField(auto_now_add=True)
     ora_sfarsit = models.TimeField(auto_now_add=True)
+    an = models.IntegerField(default=None, blank=True, null=True)
     grupa = models.CharField(max_length=5)
     tip = models.CharField(max_length=20)
-    sala = models.ForeignKey(Sala, on_delete=models.CASCADE)
-    profesor = models.ForeignKey(Profesor, on_delete=models.CASCADE)
+    sala = models.CharField(max_length=20)
+    profesor = models.CharField(max_length=60)
+    pachet = models.IntegerField(default=None, blank=True, null=True)
 
     def __str__(self):
         return self.zi
+    def get_sala(self):
+        return self.sala
--- Back/fii_student/orar/views.py
+++ Back/fii_student/orar/views.py
@@ -3,26 +3,31 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from django.template import loader
-from .models import Materie
 from .models import Rand
 from django.db.models import Sum
 import requests
 import datetime
+from datetime import time
 
 def calculate_sum(queryset):
     sum = 0
-    print(queryset)
     for i in queryset:
         sum+=i.ora_sfarsit.hour - i.ora_inceput.hour
     return sum
+
+def get_sali_unique():
+    sali_unice = {"Acvariu"}
+    toate_salile = Rand.objects.all()
+    for sala in toate_salile:
+        sali_unice.add(sala.get_sala())
+    return sorted(sali_unice)
 
 
 def index(request):
     requested_group = request.GET['grupa']
     requested_year = request.GET['an']
 
-    randuri = Rand.objects.filter(grupa = requested_group).filter(curs__an = requested_year).order_by('ora_inceput')
-
+    randuri = Rand.objects.filter(grupa = requested_group).filter(an = requested_year).order_by('ora_inceput')
     luni = randuri.filter(zi = 'Luni')
     marti = randuri.filter(zi = 'Marti')
     miercuri = randuri.filter(zi = 'Miercuri')
@@ -45,3 +50,51 @@ def index(request):
 
 
     return HttpResponse(template.render(context,request))
+
+def get_zile():
+    randuri = Rand.objects.all()
+    rand = {}
+    rand['Luni'] = luni = randuri.filter(zi='Luni')
+    rand['Marti'] = randuri.filter(zi='Marti')
+    rand['Miercuri'] = randuri.filter(zi='Miercuri')
+    rand['Joi'] = randuri.filter(zi='Joi')
+    rand['Vineri'] = randuri.filter(zi='Vineri')
+    rand['Sambata'] = randuri.filter(zi='Sambata')
+    rand['Duminica'] = randuri.filter(zi='Duminica')
+    return rand
+
+def verificare_disp(_sala, zi, i):
+    sali = zi.filter(sala = _sala)
+    t_inceput = time(i)
+    t_sfarsit = time(i+1)
+    for sala in sali:
+        if(t_sfarsit<=sala.ora_sfarsit and t_sfarsit>sala.ora_inceput or t_inceput>=sala.ora_inceput and t_inceput<sala.ora_sfarsit):
+            return 0
+        else:
+            continue
+    return 1
+
+def program_sali(request):
+    zile = ["Luni","Marti","Miercuri","Joi","Vineri","Sambata","Duminica"]
+    rand = get_zile()
+    sali = get_sali_unique()
+    ore = [time(8),time(9),time(10),time(11),time(12),time(13),time(14),time(15),time(16),time(17),time(18),time(19),time(20)]
+    template = loader.get_template('sali_disponibile.html')
+    dict = []
+    for sala in range(len(sali)):
+        dict2 = []
+        dict.append(dict2)
+        for zi in range(0,7):
+            dict3 = []
+            dict[sala].append(dict3)
+            for ora in range(0,12):
+                if verificare_disp(sali[sala],rand[zile[zi]],ora+8)==1:
+                    dict[sala][zi].append("green")
+                else:
+                    dict[sala][zi].append("red")
+
+    context = {'lista': dict,'sali': sali, 'zile': zile, 'ore': range(8,20),'lungime': len(sali), 'rangesali':range(len(sali)),'rangezile':range(7),'rangeore':range(12)}
+
+    return HttpResponse(template.render(context, request))
+
+
